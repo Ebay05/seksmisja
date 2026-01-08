cb0e2af1b5b5cddc9930b72baefab56b7ec22ffd
Ebay05 <ebay11990011@gmail.com>
2026-01-08 23:59:28 +0100

    Shadcn test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'node:path'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
-import Components from 'unplugin-vue-components/vite'
-import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
-import AutoImport from 'unplugin-auto-import/vite'
-
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-      dts: 'src/components.d.ts',
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: 'src/auto-imports.d.ts',
-    }),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
