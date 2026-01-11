2ec748fde9557ddc06b18268564b8bd58e80f1ea
Ebay05 <ebay11990011@gmail.com>
2026-01-11 20:42:13 +0100

    Fixed router 2

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -2,10 +2,6 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
-// import { useRouter } from 'vue-router'
-
-// const router = useRouter()
-
 </script>
 
 <template>
