832e80c33efac920d120933eee0fd6b0528a545a
Ebay05 <ebay11990011@gmail.com>
2026-01-11 22:27:03 +0100

    Fixed registration displaying

--- public/Homepage.vue
+++ public/Homepage.vue
@@ -1,61 +1,62 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { ref } from 'vue'
-import { supabase } from '@/lib/supabaseClient'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ref } from "vue";
+import { supabase } from "@/lib/supabaseClient";
 
-const isRegisterVisible = ref(false)
-const currentStep = ref('home')
+const isRegisterVisible = ref(false);
+const currentStep = ref("home");
 
-const email = ref('')
-const username = ref('')
-const password = ref('')
-const confirmPass = ref('')
-const isLoading = ref(false)
-const otpCode = ref('')
+const email = ref("");
+const username = ref("");
+const password = ref("");
+const confirmPass = ref("");
+const isLoading = ref(false);
+const otpCode = ref("");
 
 // FIRST REGISTER INPUT
 const handleRegister = () => {
-  if (email.value.trim() !== '' && email.value.includes('@')) {
-    currentStep.value = 'register'
+  if (email.value.trim() !== "" && email.value.includes("@")) {
+    currentStep.value = "register";
   } else {
-    alert('Proszę podać prawidłowy adres e-mail.')
+    alert("Proszę podać prawidłowy adres e-mail.");
   }
-}
+};
 
 // TO DATABASE
 const submitToSupabase = async () => {
   // Pass Validation
-  const usernameRegex = /^(?=[^0-9])(?=(.*[a-zA-Z]){3,})[a-zA-Z0-9_ ]+$/
+  const usernameRegex = /^(?=[^0-9])(?=(.*[a-zA-Z]){3,})[a-zA-Z0-9_ ]+$/;
 
-  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/
+  const strongPasswordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
 
   if (!usernameRegex.test(username.value)) {
-    return
+    return;
   }
 
   if (!strongPasswordRegex.test(password.value)) {
-    return
+    return;
   }
 
   if (password.value !== confirmPass.value) {
-    return
+    return;
   }
 
-  isLoading.value = true
+  isLoading.value = true;
 
   if (!usernameRegex.test(username.value)) {
     alert(
-      'Nazwa użytkownika jest nieprawidłowa! Nie może zaczynać się od cyfry, musi mieć min. 3 litery i może zawierać tylko spacje oraz podkreślnik (_).',
-    )
-    isLoading.value = false
-    return
+      "Nazwa użytkownika jest nieprawidłowa! Nie może zaczynać się od cyfry, musi mieć min. 3 litery i może zawierać tylko spacje oraz podkreślnik (_)."
+    );
+    isLoading.value = false;
+    return;
   }
 
   if (password.value !== confirmPass.value) {
-    alert('Hasła nie są identyczne!')
-    isLoading.value = false
-    return
+    alert("Hasła nie są identyczne!");
+    isLoading.value = false;
+    return;
   }
 
   // Supabase Auth Registration
@@ -67,48 +68,52 @@ const submitToSupabase = async () => {
         display_name: username.value,
       },
     },
-  })
+  });
 
   if (error) {
-    alert('Błąd: ' + error.message)
-    isLoading.value = false
+    alert("Błąd: " + error.message);
+    isLoading.value = false;
   } else {
     // Switch to OTP step after successful registration
-    currentStep.value = 'otp'
-    isLoading.value = false
+    currentStep.value = "otp";
+    isLoading.value = false;
   }
 
-  isLoading.value = false
-}
+  isLoading.value = false;
+};
 
 // Function to verify the 8-digit OTP code
 const verifyOtp = async () => {
-  if (otpCode.value.length < 8) return
+  if (otpCode.value.length < 8) return;
 
-  isLoading.value = true
+  isLoading.value = true;
 
   // Supabase OTP Verification
   const { error } = await supabase.auth.verifyOtp({
     email: email.value,
     token: otpCode.value,
-    type: 'signup',
-  })
+    type: "signup",
+  });
 
   if (error) {
-    alert('Błąd weryfikacji: ' + error.message)
+    alert("Błąd weryfikacji: " + error.message);
   } else {
-    alert('Konto zweryfikowane pomyślnie!')
-    currentStep.value = 'home'
+    alert("Konto zweryfikowane pomyślnie!");
+    currentStep.value = "home";
   }
 
-  isLoading.value = false
-}
+  isLoading.value = false;
+};
 </script>
 
 <template>
   <nav class="flex w-full items-center justify-between px-20 py-8">
     <RouterLink to="/">
-      <img src="/src/assets/seksmisja-logo-main.svg" alt="Logo Seksmisja" class="h-10" />
+      <img
+        src="/src/assets/seksmisja-logo-main.svg"
+        alt="Logo Seksmisja"
+        class="h-10"
+      />
     </RouterLink>
     <Button variant="outline" as-child>
       <RouterLink to="/logowanie">Zaloguj</RouterLink>
@@ -130,7 +135,12 @@ const verifyOtp = async () => {
         </p>
 
         <div class="mx-auto flex w-full max-w-md items-center space-x-2">
-          <Input type="email" placeholder="Wpisz swój e-mail..." class="h-10" v-model="email" />
+          <Input
+            type="email"
+            placeholder="Wpisz swój e-mail..."
+            class="h-10"
+            v-model="email"
+          />
 
           <!-- REGISTER BUTTON -->
           <Button
@@ -144,37 +154,54 @@ const verifyOtp = async () => {
         </div>
 
         <!-- FEATURES -->
-        <div class="mt-20 flex flex-col flex-wrap items-stretch justify-center gap-6 md:flex-row">
+        <div
+          class="mt-20 flex flex-col flex-wrap items-stretch justify-center gap-6 md:flex-row"
+        >
           <div
             class="bg-card transition-hover flex w-full flex-col items-center rounded-xl border p-6 shadow-sm hover:shadow-md md:w-[400px]"
           >
-            <img src="/src/assets/subtitles.svg" alt="" width="32px" height="32px" />
+            <img
+              src="/src/assets/subtitles.svg"
+              alt=""
+              width="32px"
+              height="32px"
+            />
             <!-- <h3 class="mb-2 font-bold text-lg">Bez limitów</h3> -->
             <p class="text-md text-muted-foreground pt-4">
-              Korzystanie z portalu jest całkowicie bezpłatne i nie nakładamy żadnych limitów na
-              liczbę wiadomości.
+              Korzystanie z portalu jest całkowicie bezpłatne i nie nakładamy
+              żadnych limitów na liczbę wiadomości.
             </p>
           </div>
 
           <div
             class="bg-card transition-hover flex w-full flex-col items-center rounded-xl border p-6 shadow-sm hover:shadow-md md:w-[400px]"
           >
-            <img src="/src/assets/shield.svg" alt="" width="32px" height="32px" />
+            <img
+              src="/src/assets/shield.svg"
+              alt=""
+              width="32px"
+              height="32px"
+            />
             <!-- <h3 class="mb-2 font-bold text-lg">Pełna prywatność</h3> -->
             <p class="text-md text-muted-foreground pt-4">
-              Chronimy Twoje dane, zdjęcia i filmy – Twój content jest zawsze pod pełną kontrolą.
-              Twoja prywatność to nasz priorytet.
+              Chronimy Twoje dane, zdjęcia i filmy – Twój content jest zawsze
+              pod pełną kontrolą. Twoja prywatność to nasz priorytet.
             </p>
           </div>
 
           <div
             class="bg-card transition-hover flex w-full flex-col items-center rounded-xl border p-6 shadow-sm hover:shadow-md md:w-[400px]"
           >
-            <img src="/src/assets/layout-dashboard.svg" alt="" width="32px" height="32px" />
+            <img
+              src="/src/assets/layout-dashboard.svg"
+              alt=""
+              width="32px"
+              height="32px"
+            />
             <!-- <h3 class="mb-2 font-bold text-lg">Intuicyjny UI</h3> -->
             <p class="text-md text-muted-foreground pt-4">
-              Doskonalimy interfejs użytkownika tak, aby był nie tylko przejrzysty, ale i intuicyjny
-              w obsłudze.
+              Doskonalimy interfejs użytkownika tak, aby był nie tylko
+              przejrzysty, ale i intuicyjny w obsłudze.
             </p>
           </div>
         </div>
@@ -183,16 +210,22 @@ const verifyOtp = async () => {
     <div v-else-if="currentStep === 'register'" key="register">
       <!-- REGISTER SECTION -->
       <div
-        v-if="isRegisterVisible"
         class="flex w-full flex-1 flex-col items-center justify-center px-12 py-10 text-center"
       >
-        <h2 class="mb-6 text-6xl font-extrabold tracking-wide lg:text-7xl">Rejestracja</h2>
+        <h2 class="mb-6 text-6xl font-extrabold tracking-wide lg:text-7xl">
+          Rejestracja
+        </h2>
         <p class="text-muted-foreground mx-auto mb-10 max-w-[600px] text-xl">
           Jeden krok dzieli cię od niezwykłej przygody
         </p>
 
         <div class="mx-auto flex w-full max-w-md flex-col items-center gap-5">
-          <Input v-model="email" type="email" placeholder="Wpisz email" class="h-10" />
+          <Input
+            v-model="email"
+            type="email"
+            placeholder="Wpisz email"
+            class="h-10"
+          />
 
           <div class="w-full text-left">
             <Input
@@ -232,15 +265,22 @@ const verifyOtp = async () => {
             </ul>
           </div>
           <div class="w-full text-left">
-            <Input v-model="password" type="password" placeholder="Stwórz hasło" class="h-10" />
+            <Input
+              v-model="password"
+              type="password"
+              placeholder="Stwórz hasło"
+              class="h-10"
+            />
 
             <ul class="mt-2 ml-1 space-y-1 text-[11px] transition-all">
               <li
                 :class="
-                  password.length >= 8 ? 'font-medium text-green-500' : 'text-muted-foreground'
+                  password.length >= 8
+                    ? 'font-medium text-green-500'
+                    : 'text-muted-foreground'
                 "
               >
-                {{ password.length >= 8 ? '✓' : '•' }} Minimum 8 znaków
+                {{ password.length >= 8 ? "✓" : "•" }} Minimum 8 znaków
               </li>
               <li
                 :class="
@@ -249,15 +289,19 @@ const verifyOtp = async () => {
                     : 'text-muted-foreground'
                 "
               >
-                {{ /[a-z]/.test(password) && /[A-Z]/.test(password) ? '✓' : '•' }} Małe i wielkie
-                litery
+                {{
+                  /[a-z]/.test(password) && /[A-Z]/.test(password) ? "✓" : "•"
+                }}
+                Małe i wielkie litery
               </li>
               <li
                 :class="
-                  /\d/.test(password) ? 'font-medium text-green-500' : 'text-muted-foreground'
+                  /\d/.test(password)
+                    ? 'font-medium text-green-500'
+                    : 'text-muted-foreground'
                 "
               >
-                {{ /\d/.test(password) ? '✓' : '•' }} Przynajmniej jedna cyfra
+                {{ /\d/.test(password) ? "✓" : "•" }} Przynajmniej jedna cyfra
               </li>
               <li
                 :class="
@@ -266,14 +310,19 @@ const verifyOtp = async () => {
                     : 'text-muted-foreground'
                 "
               >
-                {{ /[!@#$%^&*(),.?&quot;:{}|<>]/.test(password) ? '✓' : '•' }} Znak specjalny (np.
-                !, @, #, $)
+                {{ /[!@#$%^&*(),.?&quot;:{}|<>]/.test(password) ? "✓" : "•" }}
+                Znak specjalny (np. !, @, #, $)
               </li>
             </ul>
           </div>
 
           <div class="w-full text-left">
-            <Input v-model="confirmPass" type="password" placeholder="Powtórz hasło" class="h-10" />
+            <Input
+              v-model="confirmPass"
+              type="password"
+              placeholder="Powtórz hasło"
+              class="h-10"
+            />
             <p
               v-if="confirmPass && password !== confirmPass"
               class="mt-1 ml-1 animate-pulse text-[11px] text-red-500"
@@ -281,7 +330,9 @@ const verifyOtp = async () => {
               Hasła nie są identyczne
             </p>
             <p
-              v-if="confirmPass && password === confirmPass && password.length > 0"
+              v-if="
+                confirmPass && password === confirmPass && password.length > 0
+              "
               class="mt-1 ml-1 text-[11px] text-green-500"
             >
               ✓ Hasła są identyczne
@@ -295,7 +346,7 @@ const verifyOtp = async () => {
             @click="submitToSupabase"
             :disabled="isLoading"
           >
-            {{ isLoading ? 'Tworzenie konta...' : 'Zarejestruj' }}
+            {{ isLoading ? "Tworzenie konta..." : "Zarejestruj" }}
           </Button>
         </div>
       </div>
@@ -306,7 +357,9 @@ const verifyOtp = async () => {
       key="otp"
       class="flex w-full flex-1 flex-col items-center justify-center px-12 py-10 text-center"
     >
-      <h2 class="mb-6 text-6xl font-extrabold tracking-wide lg:text-7xl">Weryfikacja</h2>
+      <h2 class="mb-6 text-6xl font-extrabold tracking-wide lg:text-7xl">
+        Weryfikacja
+      </h2>
       <p class="text-muted-foreground mx-auto mb-10 max-w-[600px] text-xl">
         Wysłaliśmy 8-cyfrowy kod na adres <br />
         <span class="text-primary font-bold">{{ email }}</span>
@@ -326,7 +379,7 @@ const verifyOtp = async () => {
           @click="verifyOtp"
           :disabled="isLoading || otpCode.length < 8"
         >
-          {{ isLoading ? 'Weryfikacja...' : 'Potwierdź kod' }}
+          {{ isLoading ? "Weryfikacja..." : "Potwierdź kod" }}
         </Button>
 
         <button
